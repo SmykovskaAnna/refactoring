9959e9f03db2112ddabd4456a14ccfd77a3d0890
zipz231_sav <zipz231_sav@student.ztu.edu.ua>
2025-01-29 20:14:17 +0100

    Hardcoded strings moved to resources

--- wcf_chat/ServiceChat.cs
+++ wcf_chat/ServiceChat.cs
@@ -15,17 +15,21 @@ namespace wcf_chat
         List<ServerUser> users = new List<ServerUser>();
         int nextId = 1;
 
+        public static class Messages
+        {
+            public const string UserConnected = "{0} підключився до чату!";
+            public const string UserDisconnected = "{0} покинув чат!";
+        }
+
         public int Connect(string name)
         {
-
             ServerUser user = new ServerUser() {
                 ID = nextId,
                 Name = name,
                 operationContext = OperationContext.Current
             };
             nextId++;
-
-            SendMsg(": " + user.Name + " підключився до чату!",0);
+            SendMsg(string.Format(Messages.UserConnected, user.Name), 0);
             users.Add(user);  
             return user.ID;
         }
@@ -36,7 +40,7 @@ namespace wcf_chat
             if (user!=null)
             {
                 users.Remove(user);
-                SendMsg(": " + user.Name + " покинув чат!",0);
+                SendMsg(string.Format(Messages.UserDisconnected, user.Name), 0);
             }
         }
 
